294333e2e5f882a811b9511e85d057a1e62fbf70
James Ngugi <ngugi823@gmail.com>
2016-07-21 15:11:27 +0300

    A few refactors

---
+++ sessions/config.php
@@ -0,0 +1,4 @@
+<?php
+// Specify your login credentials
+$username = "WeloveNerds";
+$apikey = "3hehehehehehedbb55caa29304303f6979e56041dbd3d97a319f42d";
---
+++ sessions/sankaraDB.php
@@ -0,0 +1,19 @@
+<?php
+//Connection Credentials
+$servername = 'localhost';
+$username = 'root';
+$password = "password";
+$database = "ussd";
+$dbport = 3306;
+
+// Create connection
+$db = new mysqli($servername, $username, $password, $database, $dbport);
+
+// Check connection
+if ($db->connect_error) {
+    header('Content-type: text/plain');
+
+    //log error to file/db $e-getMessage()
+    die("END An error was encountered. Please try again later");
+}
+//die("Connected successfully (" . $db->host_info . ")");
